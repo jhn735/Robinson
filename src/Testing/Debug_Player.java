ce4f996308912c97bcf25526ebf744900cb962cf
John Villa <jhn735@gmail.com>
2015-02-02 19:48:30 -0800

    Player.fromJSONObject() tested and works.

--- src/Testing/Debug_Player.java
+++ src/Testing/Debug_Player.java
@@ -4,6 +4,7 @@ package Testing;
 import ListElements.Player;
 import java.util.ArrayList;
 import java.util.List;
+import org.json.simple.JSONObject;
 
 /**
  * Debug the player's methods here
@@ -13,13 +14,14 @@ public class Debug_Player {
         public static ArrayList<Player> pList = new ArrayList<>();
     public static void main(String[] args){
     
-        //set up list of players
+        /*//set up list of players
         for(int i = 0; i < 10; i++){
             System.out.println(i);
             Player p = new Player(10-i, "Player#"+Integer.toString(10-i));
             p.setRank((double)(i));
             pList.add(p);
-        } 
+        } */
+        
         /*test the sorting of players by rank
         Debug_Player.printPlayerList(pList);
 
@@ -27,10 +29,22 @@ public class Debug_Player {
         System.out.println();
         Debug_Player.printPlayerList(pList);//*/
         
+        /*test out the fromJSONObject*/
+        for(int i = 0; i < 10; i++){
+            JSONObject nPlayer = new JSONObject();
+            nPlayer.put("sid", i);
+            nPlayer.put("id", i);
+            nPlayer.put("name", "Player#"+Integer.toString(i));
+            nPlayer.put("rank", (double)(10-i));
+            Player p = new Player();
+            p.fromJSONObject(nPlayer);
+            pList.add(p);
+        }
         /*test out the toJSONObject*/
         for(Player p: pList)
-            System.out.println(p.toJSONObject());
+            System.out.println((p.toJSONObject()).toJSONString());
         //*/
+        
     }
     
     public static void printPlayerList(ArrayList<Player> l){
